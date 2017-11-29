32b6d1a856a2a92b2e172507e1e2ed309458666d
Bob Greenshields <bob@thegreenshields.com>
2017-11-29 14:35:07 +0000

    add new setup script

---
+++ new-chef-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# down load the chef client from https://downloads.chef.io/chef
+#
+# and install it with sudo dpkg -i <filename.deb>
+
+sudo apt-get install -y nfs-common
+mkdir /home/bobg/chef
+touch /home/bobg/chef/config.rb
+echo "cookbook_path ['/home/bobg/chef/cookbooks']" > /home/bobg/chef/config.rb
+sudo mkdir -p /mnt/nfs/vendor
+sudo mount 172.16.77.101:/mnt/secure/vendor /mnt/nfs/vendor
+ln -s /mnt/nfs/vendor /home/bobg/vendor
+ln -s /mnt/nfs/vendor/defiant/cookbooks /home/bobg/chef/cookbooks
